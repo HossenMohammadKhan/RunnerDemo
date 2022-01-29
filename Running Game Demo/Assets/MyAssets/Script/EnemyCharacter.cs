79f7875322a4f8f99665244d3804f8d343d7afb8
HossenMohammadKhan <98544094+HossenMohammadKhan@users.noreply.github.com>
2022-01-29 10:57:52 +0600

    0.00.07

--- Running Game Demo/Assets/MyAssets/Script/EnemyCharacter.cs
+++ Running Game Demo/Assets/MyAssets/Script/EnemyCharacter.cs
@@ -2,29 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObsticleEnemy : MonoBehaviour
+public class EnemyCharacter : MonoBehaviour
 {
+    Animator animator;
+    CapsuleCollider capsuleCollider;
+    Rigidbody rb;
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        capsuleCollider = gameObject.GetComponent<CapsuleCollider>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "ChildSingle")
         {
             other.gameObject.GetComponent<Animator>().SetTrigger("isDead");
+            animator.SetTrigger("isDead");
             other.gameObject.GetComponent<ChildMovement>().Speed = 0f;
             Destroy(other.gameObject, 2f);
+            Destroy(gameObject, 2f);
+            capsuleCollider.enabled = false;
+            rb.useGravity = false;
+            rb.isKinematic = true;
         }
         else if (other.gameObject.tag == "Player")
         {
             other.gameObject.GetComponent<Animator>().SetTrigger("isDead");
+            animator.SetTrigger("isDead");
             other.gameObject.GetComponent<PlayerMovementDemoRunner>().Speed = 0f;
             Destroy(other.gameObject, 2f);
+            Destroy(gameObject, 2f);
+            capsuleCollider.enabled = false;
+            rb.useGravity = false;
+            rb.isKinematic = true;
         }
         else if (other.gameObject.tag == "GroupChild")
         {
             other.gameObject.GetComponent<Animator>().SetTrigger("isDead");
+            animator.SetTrigger("isDead");
             other.gameObject.GetComponent<GroupChildMovement>().Speed = 0f;
             Destroy(other.gameObject, 2f);
+            Destroy(gameObject, 2f);
+            capsuleCollider.enabled = false;
+            rb.useGravity = false;
+            rb.isKinematic = true;
         }
     }
-
-
 }
--- Running Game Demo/Assets/MyAssets/Script/FightManagement.cs
+++ Running Game Demo/Assets/MyAssets/Script/FightManagement.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 public class FightManagement : MonoBehaviour
 {
-    private float speed = 2f;
+    private float speed = 10f;
     [SerializeField] GameObject[] Players, Enemies;
 
 
@@ -14,22 +14,42 @@ public class FightManagement : MonoBehaviour
     }
 
     // Update is called once per frame
+    private bool isMoved1, isMoved2, isMoved3 = false;
+
     void Update()
     {
-        foreach (GameObject player in Players)
+
+        foreach (GameObject Enemy in Enemies)
         {
-            foreach (GameObject Enemy in Enemies)
+            //var currentPlayer = player;
+            foreach (GameObject player in Players)
             {
                 if (player != null && Enemy != null)
                 {
                     float step = speed * Time.deltaTime;
                     float distance = Vector3.Distance(player.transform.position, Enemy.transform.position);
-                    if (distance < 10f)
+                    if (distance < 10f && distance > 1 && isMoved1 == false)
                     {
-                        if (distance > 3)
+                        player.transform.position = Vector3.MoveTowards(player.transform.position, Enemy.transform.position, step);
+                        if (distance < 1)
                         {
-                            player.transform.position = Vector3.MoveTowards(player.transform.position, Enemy.transform.position, step);
-                            Enemy.transform.position = Vector3.MoveTowards(Enemy.transform.position, player.transform.position, step);
+                            //isMoved1 = true;
+                            player.transform.position = Vector3.MoveTowards(player.transform.position, Enemy.transform.position, 0);
+                            Enemy.transform.position = Vector3.MoveTowards(Enemy.transform.position, player.transform.position, 0);
+                            if (player.tag == "ChildSingle")
+                            {
+                                player.gameObject.GetComponent<ChildMovement>().Speed = 0;
+                            }
+                            else if (player.tag == "Player")
+                            {
+                                player.gameObject.GetComponent<PlayerMovementDemoRunner>().Speed = 0;
+                                //player.gameObject.GetComponent<Animator>().SetTrigger("isDead");
+                            }
+                            else if (player.tag == "GroupChild")
+                            {
+                                player.gameObject.GetComponent<GroupChildMovement>().Speed = 0;
+                                //player.gameObject.GetComponent<Animator>().SetTrigger("isDead");
+                            }
                         }
                     }
                 }
